73e3b746eeec367d2db84017f26a373d59528c75
Maria-knst <fpm.konstantMA@bsu.by>
2022-07-28 11:15:32 +0300

    Made input form skeleton цше.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="FormTitle">Form</h1>
+        <form className="MyForm">
+          <input className="MyFormInput" type="text" pattern="[A-Za-z]{3,30}\s[A-Za-z]{3,30}" style={{textTransform: 'uppercase'}} placeholder="Full Name"/>
+          <input className="MyFormInput" type="text" pattern="[\w-]+@[a-z]+.[a-z]+" placeholder="E-mail"/>
+          <input className="MyFormInput" type="tel" placeholder="+7 (987) 654-32-10"/>
+          <input className="MyFormInput" type="date" placeholder="Your Birthday"/>
+          <textarea name="message" id="MyFormMessage" minLength="10" maxLength="300" placeholder="Message" cols="30" rows="10"></textarea>
+          <button> Send Me</button>
+        </form>
     </div>
   );
 }
